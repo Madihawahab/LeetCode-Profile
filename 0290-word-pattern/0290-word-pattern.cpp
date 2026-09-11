5006e29cad6caa71b658b3f943aaca64a68dca3c
Madihawahab <Madihawahab2111@gmail.com>
2026-09-11 15:33:30 +0530

    Time: 0 ms (100%), Space: 8.9 MB (40.36%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     bool wordPattern(string pattern, string s) {
 
-        vector<string> words;
+        unordered_map<char, int> charToIndex;
+        unordered_map<string, int> wordToIndex;
+
         stringstream ss(s);
-
         string token;
-        int countWords = 0;
 
-        while(getline(ss, token, ' ')){
-            words.push_back(token);
-            countWords++;
-        }
-        
-        int n = pattern.length();
+        int countTokens = 0;
+        int i = 0;
+        int n = pattern.size();
 
-        if(n != countWords){
-            return false;
-        }
+        while(ss >> token){
+            countTokens++;
 
-        unordered_map<string, char> mp;
-        set<char> used;
-
-        for(int i = 0; i<n; i++){
-            string word = words[i];
-            char ch = pattern[i];
-
-            if(mp.find(word) == mp.end() && used.find(ch) == used.end()){
-                used.insert(ch);
-                mp[word] = ch;
-            }else if(mp[word] != pattern[i]){
+            if(i == n || charToIndex[pattern[i]] != wordToIndex[token]){
                 return false;
             }
+
+            charToIndex[pattern[i]] = i+1;
+            wordToIndex[token] = i+1;
+
+            i++;
+        }
+
+        if(countTokens != n || i != n){
+            return false;
         }
 
         return true;
