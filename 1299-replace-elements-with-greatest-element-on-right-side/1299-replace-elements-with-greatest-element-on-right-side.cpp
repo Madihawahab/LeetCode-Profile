1f4164819e700256bfd91806ce0669663fb90a47
Madihawahab <Madihawahab2111@gmail.com>
2026-09-22 10:18:09 +0530

    Time: 1623 ms (13.79%), Space: 72.7 MB (74.31%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -4,28 +4,20 @@ public:
         
         int n = arr.size();
 
-        int grt;
+        
         int i, j;
-        for(i = 0; i<n; i++){
-            if(i<n-1){
-                grt = arr[i+1];
-            for(j = i; j<n; j++){
-                if(j<n-1){
-                    if(grt<arr[j+1]){
-                    grt = arr[j+1];
+        for(i = 0; i<n-1; i++){
+            int grt = INT_MIN;
+            for(j = i+1; j<n; j++){
+                if(grt<=arr[j]){
+                    grt = arr[j];
                 }
-                }
-                
             }
-            if(j == n){
-                arr[i] = grt;
-            }
-            } else{
+            arr[i] = grt;
+        }
+        if(i == n-1){
             arr[i] = -1;
         }
-            
-        }
-       
 
         return arr;
     }
