3502b892331aa8d9f92ad98d0c1d866fbe9ce21e
Madihawahab <Madihawahab2111@gmail.com>
2026-09-22 11:00:35 +0530

    Time: 87 ms (17.7%), Space: 113.4 MB (16.34%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,16 +3,20 @@ public:
     bool containsDuplicate(vector<int>& nums) {
 
         int n = nums.size();
-        
+
         unordered_map<int, int> mp;
 
-        for(int i = 0; i<n; i++){
-            if(mp.find(nums[i]) == mp.end()){
-                mp[nums[i]]++;
-            }else{
+        for(auto &it : nums){
+            mp[it]++;
+        }
+
+        for(auto &it : mp){
+            if(it.second>1){
                 return true;
             }
         }
+
         return false;
+        
     }
 };
