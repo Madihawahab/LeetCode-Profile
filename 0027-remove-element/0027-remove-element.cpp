b44fd5ca87bd9f6b55fcbf105f3b1167aaa6e0b8
Madihawahab <Madihawahab2111@gmail.com>
2026-09-23 16:07:21 +0530

    Time: 0 ms (100%), Space: 11.7 MB (83.89%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -4,28 +4,19 @@ public:
 
         int n = nums.size();
 
-        //optimal approach
-        //time complexity : O(n)
-        //space complexity : O(1)
-
         vector<int> ans;
 
-        int k = 0;
-        int del = 0;
         for(int i = 0; i<n; i++){
             if(val != nums[i]){
-                nums[k] = nums[i];
-                k += 1;
-            }else{
-                del++;
+                ans.push_back(nums[i]);
             }
         }
-
-        for(int i = 0; i<del; i++){
-            nums.pop_back();
-        }
-
         
+        nums.clear();
+
+        for(int i = 0; i<ans.size(); i++){
+            nums.push_back(ans[i]);
+        }
         return nums.size();
     }
 };
