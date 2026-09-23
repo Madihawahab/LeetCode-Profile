7ceb9f4165670a5c21280ba00adec917c4b5f493
Madihawahab <Madihawahab2111@gmail.com>
2026-09-23 18:41:59 +0530

    Time: 0 ms (100%), Space: 23.7 MB (75.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,25 +3,56 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
 
         int n = nums.size();
-        //Brute Force
+        //optimal appraoch
         //time complexity : O(n)
-        //space somplexity : O(n)
+        //space somplexity : O(1)
 
-        unordered_map<int, int> mp;
+        int count1 = 0; 
+        int maj1 = NULL;
 
-        for(auto &it : nums){
-            mp[it]++;
-        }
+        int count2 = 0;
+        int maj2 = NULL;
 
-        vector<int> ans;
-
-        for(auto &it : mp){
-            if(it.second > (n/3)){
-                ans.push_back(it.first);
+        for(int i = 0; i<n; i++){
+            if(nums[i] == maj1){
+                count1++;
+            }else if(nums[i] == maj2){
+                count2++;
+            }else if(count1 == 0){
+                maj1 = nums[i];
+                count1 = 1;
+            }else if(count2 == 0){
+                maj2 = nums[i];
+                count2 = 1;
+            }else{
+                count1--;
+                count2--;
             }
         }
 
-        return ans;
+        //verification
+
+        vector<int> result;
+        int freq1 = 0;
+        int freq2 = 0;
+
+        for(int &num : nums){
+            if(num == maj1){
+                freq1++;
+            }else if(num == maj2){
+                freq2++;
+            }
+        }
+
+        if(freq1>floor(n/3)){
+            result.push_back(maj1); 
+        }
+
+        if(freq2>floor(n/3)){
+            result.push_back(maj2);
+        }
+
+        return result;
         
     }
 };
