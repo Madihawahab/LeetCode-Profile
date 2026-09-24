ad09f3c14244f65f1dc41b6ae95f42bae58fe8cf
Madihawahab <Madihawahab2111@gmail.com>
2026-09-24 11:41:56 +0530

    Time: 15 ms (90.69%), Space: 70.4 MB (55.01%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,21 +1,50 @@
 class MyHashMap {
 public:
-vector<int> vec;
+    vector<list<pair<int, int>>> bucket;
+    int size = 10000;
+
     MyHashMap() {
-        vec.resize(1000001, -1);
+        bucket.resize(size);
     }
     
     void put(int key, int value) {
-        vec[key] = value;
+       int bucket_no = key%size;
+       auto &chain = bucket[bucket_no];
+
+       for(auto &it : chain){
+        if(it.first == key){
+            it.second = value;
+            return;
+        }
+       }
+       chain.emplace_back(key, value);
     }
     
     int get(int key) {
-        
-        return vec[key];
+        int bucket_no = key%size;
+        auto &chain = bucket[bucket_no];
+        if(chain.empty()){
+            return -1;
+        }
+
+        for(auto &it : chain){
+            if(it.first == key){
+                return it.second;
+            }
+        }
+        return -1;
     }
     
     void remove(int key) {
-        vec[key] = -1;
+        int bucket_no = key%size;
+        auto &chain = bucket[bucket_no];
+
+        for(auto it = chain.begin(); it != chain.end(); it++){
+            if(it->first == key){
+                chain.erase(it);
+                return;
+            }
+        }
     }
 };
 
