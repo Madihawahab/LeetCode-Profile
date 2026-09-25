48b15001980592a31fe2ccfae91ca3371f771eb1
Madihawahab <Madihawahab2111@gmail.com>
2026-09-25 20:18:07 +0530

    Time: 3 ms (47.16%), Space: 25.7 MB (14.33%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,68 +1,26 @@
 class Solution {
 public:
-
-    bool validBox(vector<vector<char>>& board, int sr, int er, int sc, int ec){
-        unordered_set<char> st;
-
-        for(int i = sr; i<=er; i++){
-            for(int j = sc; j<=ec; j++){
-                if(board[i][j] == '.') continue;
-
-                if(st.find(board[i][j]) != st.end()){
-                    return false;
-                }
-
-                st.insert(board[i][j]);
-            }
-        }
-        return true;
-    }
-
     bool isValidSudoku(vector<vector<char>>& board) {
 
-        for(int r = 0; r<9; r++){
-            unordered_set<char> st;
-            for(int c = 0; c<9; c++){
-                if(board[r][c] == '.'){
-                    continue;
-                }
+        unordered_set<string> st;
 
-                if(st.find(board[r][c]) != st.end()){
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                if(board[i][j] == '.') continue;
+
+                string row = string(1, board[i][j]) + "_ROW_" + to_string(i);
+                string col = string(1, board[i][j]) + "_COL_" + to_string(j);
+                string box = string(1, board[i][j]) + "_BOX_" + to_string(i/3) + "_" + to_string(j/3);
+
+                if(st.find(row) != st.end() || st.find(col) != st.end() || st.find(box) != st.end()){
                     return false;
                 }
 
-                st.insert(board[r][c]);
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
-
-         for(int c = 0; c<9; c++){
-            unordered_set<char> st;
-            for(int r = 0; r<9; r++){
-                if(board[r][c] == '.'){
-                    continue;
-                }
-
-                if(st.find(board[r][c]) != st.end()){
-                    return false;
-                }
-
-                st.insert(board[r][c]);
-            }
-        }
-
-        for(int sr = 0; sr<9; sr+=3){
-            int er = sr+2;
-            for(int sc = 0; sc<9; sc+=3){
-                int ec = sc+2;
-
-                //sr, er, sc, ec
-                if(!validBox(board, sr, er, sc, ec)){
-                    return false;
-                }
-            }
-        }
-
         return true;
-        
     }
 };
